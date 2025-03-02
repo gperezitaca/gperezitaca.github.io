9a27912b498b6a35fcd5a6fcd70d2ab54fabfecd
Guillermo <gperez@ies-itaca.es>
2025-03-02 21:24:45 +0100

    Ultimos cambios

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const LINKS = {
   github: "https://github.com/gperezitaca"
   linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
   medium: "https://gperezitaca.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  discord: "https://discordappp.com/users/163300027618295808",
   dribble: "https://dribbble.com/Ladvace_Jace",
   email: "contact@gianmarcocavallo.com<",
 };
