50bbe409a47d4a60ed2a55e972194d269a6d2a2d
alumno <alumno@vbox.Home>
2025-03-02 21:21:05 +0100

    Ultimos cambios

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const LINKS = {
   github: "https://github.com/gperezitaca"
   linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
+  medium: "https://gperezitaca.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
   dribble: "https://dribbble.com/Ladvace_Jace",
   email: "contact@gianmarcocavallo.com<",
